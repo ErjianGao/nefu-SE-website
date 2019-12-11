21ceaba7aaa292667d6107fca90db548cf9b4b48
Eric Gao <754095810@qq.com>
2019-12-12 01:49:51 +0800

    add web listener

--- src/main/java/com/util/DataSourceUtils.java
+++ src/main/java/com/util/DataSourceUtils.java
@@ -1,7 +1,8 @@
-package com.Util;
+package com.util;
 
 import javax.annotation.Resource;
 import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
@@ -9,6 +10,7 @@ import java.sql.SQLException;
 /**
  * 创建容器启动监听器DataSourceUtils，注入DataSource对象，暴露连接对象获取方法
  */
+@WebListener
 public class DataSourceUtils implements ServletContextListener {
     @Resource(name = "jdbc/MySQL")
     private static DataSource dataSource;
