7dd51fc65d7cf06b65ec004e711f200bfc46df6e
Eric Gao <754095810@qq.com>
2019-12-14 23:46:49 +0800

    add lab servlet

---
+++ src/main/java/com/controller/Lab923Servlet.java
@@ -0,0 +1,16 @@
+package com.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/lab-923")
+public class Lab923Servlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/WEB-INF/jsp/lab-923.jsp").forward(req,resp);
+    }
+}
---
+++ src/main/java/com/controller/Lab925Servlet.java
@@ -0,0 +1,16 @@
+package com.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/lab-925")
+public class Lab925Servlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/WEB-INF/jsp/lab-925.jsp").forward(req,resp);
+    }
+}
