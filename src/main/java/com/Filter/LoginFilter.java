36b1c77796eaeb2d4fc757d5468c8de21d0eff9d
Eric Gao <754095810@qq.com>
2019-12-14 23:46:17 +0800

    add filter address

--- src/main/java/com/Filter/LoginFilter.java
+++ src/main/java/com/Filter/LoginFilter.java
@@ -9,16 +9,33 @@ import javax.servlet.http.HttpFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
-@WebFilter("/backstage")
+@WebFilter("/*")
 public class LoginFilter extends HttpFilter {
+    private List<String> filterUrls = List.of("/backstage", "/add-news","/delete-news","/news-detail","/news-manage","/person-manage","/update-news","/update-person");
+
     @Override
     protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
-        Manager manager =  (Manager)req.getSession().getAttribute("manager");
-        if(manager == null) {
-            res.sendRedirect("/login");
-        } else {
+        if(req.getServletPath().equals("/login")) {
             chain.doFilter(req,res);
+            return;
         }
+
+        for (String filterUrl : filterUrls) {
+            if (filterUrl.equals(req.getServletPath())) {
+                Manager manager =  (Manager)req.getSession().getAttribute("manager");
+                if(manager == null) {
+                    //这里要注意如果一个函数中有多个请求转发或者是重定向的话，每次都需要在操作后return
+                    res.sendRedirect("/login");
+                    return;
+                }
+                else {
+                    chain.doFilter(req,res);
+                    return;
+                }
+            }
+        }
+        chain.doFilter(req,res);
     }
 }
